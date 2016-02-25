688b422f4f80ba219b059e429dfe462936e531ae
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2016-02-24 18:16:42 -0800

    Install git before cloning

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 sudo apt-get install puppet-common
 sudo puppet module install puppetlabs/firewall
+sudo apt-get install git
 sudo git clone https://github.com/pgrandin/linux-workshop.git /etc/puppet/modules/
 sudo puppet apply /etc/puppet/modules/linux-workshop/nodes.pp
