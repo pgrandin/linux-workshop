2792cf9baa4e9a3b8593e57ffe34725d3a22c3b5
Pierre Grandin <grandinp@altern.org>
2016-02-24 20:41:33 -0500

    Clone the repo during startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 sudo apt-get install puppet-common
 sudo puppet module install puppetlabs/firewall
+sudo git clone https://github.com/pgrandin/linux-workshop.git /etc/puppet/modules/
 sudo puppet apply /etc/puppet/modules/linux-workshop/nodes.pp
