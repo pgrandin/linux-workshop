525468ea21927f176ac884949862dd0dff728f8b
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2016-03-09 10:29:05 -0800

    Added wireshark to the image

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 apt-get install puppet-common
-apt-get install -y git
+apt-get install -y git fluxbox xterm wireshark vnc4server
 [ -d /etc/puppet/modules/ ] || mkdir /etc/puppet/modules/
 git clone https://github.com/pgrandin/linux-workshop.git /etc/puppet/modules/
 puppet module install puppetlabs/firewall
