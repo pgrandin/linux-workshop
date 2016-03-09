30b4d356b828fe5ab8a30bf43f0cc5d3d7b570d7
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2016-03-09 11:06:08 -0800

    Added basic socket server compilation

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 apt-get install puppet-common
-apt-get install -y git fluxbox xterm wireshark vnc4server
+apt-get install -y git fluxbox xterm wireshark vnc4server gcc
+wget https://raw.githubusercontent.com/pgrandin/linux-workshop/master/demo.c -O /usr/src/
+gcc -o /usr/local/bin/workshop-server /usr/src/demo.c
+rm /usr/local/bin/workshop-server
 [ -d /etc/puppet/modules/ ] || mkdir /etc/puppet/modules/
 git clone https://github.com/pgrandin/linux-workshop.git /etc/puppet/modules/
 puppet module install puppetlabs/firewall
