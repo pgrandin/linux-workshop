cdcfe0adfaad03b6588b0b0b53a6956e0d414bf8
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2016-03-09 11:34:08 -0800

    Added loader

--- startup.sh
+++ startup.sh
@@ -2,7 +2,10 @@ apt-get install puppet-common
 apt-get install -y git fluxbox xterm wireshark vnc4server gcc
 wget https://raw.githubusercontent.com/pgrandin/linux-workshop/master/demo.c -O /usr/src/
 gcc -o /usr/local/bin/workshop-server /usr/src/demo.c
-rm /usr/local/bin/workshop-server
+rm /usr/src/demo.c
+wget https://raw.githubusercontent.com/pgrandin/linux-workshop/master/loader.c -O /usr/src/
+gcc -o /usr/local/bin/devcon02 /usr/src/test2.c -lm
+rm /usr/src/test2.c
 [ -d /etc/puppet/modules/ ] || mkdir /etc/puppet/modules/
 git clone https://github.com/pgrandin/linux-workshop.git /etc/puppet/modules/
 puppet module install puppetlabs/firewall
