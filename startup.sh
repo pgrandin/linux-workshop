30dceff061d091d77104d6c516ae4a651cb5cd65
Pierre GRANDIN <pgrandin@users.noreply.github.com>
2016-03-09 12:23:35 -0800

    Fixed paths

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 apt-get install puppet-common
 apt-get install -y git fluxbox xterm wireshark vnc4server gcc
-wget https://raw.githubusercontent.com/pgrandin/linux-workshop/master/demo.c -O /usr/src/
+wget https://raw.githubusercontent.com/pgrandin/linux-workshop/master/demo.c -O /usr/src/demo.c
 gcc -o /usr/local/bin/workshop-server /usr/src/demo.c
 rm /usr/src/demo.c
-wget https://raw.githubusercontent.com/pgrandin/linux-workshop/master/loader.c -O /usr/src/
-gcc -o /usr/local/bin/devcon02 /usr/src/test2.c -lm
+wget https://raw.githubusercontent.com/pgrandin/linux-workshop/master/loader.c -O /usr/src/loader.c
+gcc -o /usr/local/bin/devcon02 /usr/src/loader.c -lm
 rm /usr/src/test2.c
 [ -d /etc/puppet/modules/ ] || mkdir /etc/puppet/modules/
 git clone https://github.com/pgrandin/linux-workshop.git /etc/puppet/modules/
